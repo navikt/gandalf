2c642156d8cfc81178431cebcee9b40e0ca5dc6a
yousher <youssef.bel.mekki@nav.no>
2020-06-09 00:28:31 +0200

    actuator dep. får appen til å ikke starte må undersøke hvorfor.

--- build.gradle.kts
+++ build.gradle.kts
@@ -69,7 +69,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    // implementation("org.springframework.boot:spring-boot-starter-actuator")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     implementation("org.springframework.ldap:spring-ldap-core")
     implementation("org.springframework.security:spring-security-ldap")
