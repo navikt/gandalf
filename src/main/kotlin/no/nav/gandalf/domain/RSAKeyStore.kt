d07c4cb341743ae01608590da07db43fc911e273
yousher <youssef.bel.mekki@nav.no>
2020-06-09 11:29:43 +0200

    Fikset på id attr, som ikke ble oppdatert riktig

--- src/main/kotlin/no/nav/gandalf/domain/RSAKeyStore.kt
+++ src/main/kotlin/no/nav/gandalf/domain/RSAKeyStore.kt
@@ -1,6 +1,9 @@
 package no.nav.gandalf.domain
 
 import com.nimbusds.jose.jwk.RSAKey
+import no.nav.gandalf.util.TimestampConverter
+import org.jetbrains.annotations.NotNull
+import org.json.JSONObject
 import java.time.LocalDateTime
 import javax.persistence.Column
 import javax.persistence.Convert
@@ -8,16 +11,21 @@ import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
+import javax.persistence.SequenceGenerator
 import javax.persistence.Table
-import no.nav.gandalf.util.TimestampConverter
-import org.jetbrains.annotations.NotNull
-import org.json.JSONObject
 
 @Entity
 @Table(name = "KEYSTORE")
 data class RSAKeyStore(
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) @Column(name = "ID") var id: Long? = 0,
+    @Id @SequenceGenerator(
+        name = "keyStoreIdGenerator",
+        sequenceName = "KEYSTORE_ID_SEQ",
+        allocationSize = 1,
+        initialValue = 1
+    ) @GeneratedValue(
+        strategy = GenerationType.SEQUENCE,
+        generator = "keyStoreIdGenerator"
+    ) @Column(name = "ID") var id: Long? = 0,
     @get: NotNull @Column(name = "RSAKEY", length = 2000) var rsaKey: String = "",
     @get: NotNull @Column(name = "EXPIRES") @Convert(converter = TimestampConverter::class) var expires: LocalDateTime = LocalDateTime.now()
 ) {
@@ -55,7 +63,9 @@ data class RSAKeyStore(
                 for (k in oldJson.keySet()) {
                     if (oldJson.get(k).javaClass == JSONObject::class.java) {
                         val newJson: JSONObject = oldJson.get(k) as JSONObject
-                        newKeyStr += "\"" + k + "\":\"" + (if (newJson.has("value")) newJson.get("value") else newJson.get("name")) + "\","
+                        newKeyStr += "\"" + k + "\":\"" + (if (newJson.has("value")) newJson.get("value") else newJson.get(
+                            "name"
+                        )) + "\","
                     } else if (oldJson.get(k).javaClass == String::class.java) {
                         newKeyStr += "\"" + k + "\":\"" + (if (oldJson.get(k) == "SIGNATURE") "sig" else oldJson.get(k)) + "\","
                     }
