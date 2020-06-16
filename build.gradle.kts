ecc61df901a7c2e52cf405d294287e6cedbeb59d
yousher <youssef.bel.mekki@nav.no>
2020-06-17 01:06:59 +0200

    Update dep. for opeanpi to 1.4.1 did bring in an issue with spring.test.mvc so downgraded

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,8 @@ object Version {
     const val logbackStash = "6.4"
     const val mockOAuth2Server = "0.1.34"
     const val nimbus = "8.9"
-    const val openapi = "1.4.1"
+    // Newest ver 1.4.1 brings an issue: https://github.com/springdoc/springdoc-openapi/issues/729
+    const val openapi = "1.2.32"
     const val unboundid = "5.1.0"
     const val wiremock = "2.26.3"
     const val wiremockCloud = "2.2.3.RELEASE"
