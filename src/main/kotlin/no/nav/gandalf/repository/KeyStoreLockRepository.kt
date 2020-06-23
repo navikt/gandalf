1d35b5a04456e3652b03c6d42d792200da74ec11
Youssef Bel Mekki <38552193+ybelMekk@users.noreply.github.com>
2020-06-23 23:35:42 +0200

    Roll back lock changes. (#8)

--- src/main/kotlin/no/nav/gandalf/repository/KeyStoreLockRepository.kt
+++ src/main/kotlin/no/nav/gandalf/repository/KeyStoreLockRepository.kt
@@ -2,12 +2,5 @@ package no.nav.gandalf.repository
 
 import no.nav.gandalf.domain.KeyStoreLock
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Lock
-import java.util.Optional
-import javax.persistence.LockModeType
 
-interface KeyStoreLockRepository : JpaRepository<KeyStoreLock, Long> {
-
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
-    override fun findById(id: Long): Optional<KeyStoreLock>
-}
+interface KeyStoreLockRepository : JpaRepository<KeyStoreLock, Long>
