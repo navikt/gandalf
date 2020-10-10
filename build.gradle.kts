18c64f7e7486078af687c2b8ef33b3641706a60f
Youssef Bel Mekki <38552193+ybelMekk@users.noreply.github.com>
2020-10-10 21:41:53 +0200

    dep: Dependency: (#21)
    * Kotlin -> 1.4.10
    * kotlinlogging -> 2.0.3
    * mockOAuth2Server -> 0.1.35
    * nimbus -> 8.22
    * openApi -> 1.4.8
    * wiremock 2.27.2
    Plugin:
    * kotlinter -> 3.2.0
    * ben-manes -> 0.33.0
    * spring-boot -> 2.3.4.RELEASE

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,13 +5,13 @@ object Version {
     const val apacheHttp = "4.5.12"
     const val gradleVersion = "6.5"
     const val json = "20200518"
-    const val kotlinLoggin = "1.8.3"
+    const val kotlinLoggin = "2.0.3"
     const val logbackStash = "6.4"
-    const val mockOAuth2Server = "0.1.34"
-    const val nimbus = "8.19"
-    const val openapi = "1.4.4"
+    const val mockOAuth2Server = "0.1.35"
+    const val nimbus = "8.22"
+    const val openapi = "1.4.8"
     const val unboundid = "5.1.1"
-    const val wiremock = "2.27.1"
+    const val wiremock = "2.27.2"
     const val wiremockCloud = "2.2.4.RELEASE"
 }
 
@@ -20,11 +20,11 @@ val mainClass = "no.nav.gandalf.GandalfApplication"
 plugins {
     application
     java
-    val kotlinVersion = "1.4.0"
+    val kotlinVersion = "1.4.10"
     kotlin("plugin.allopen") version kotlinVersion
-    id("org.jmailen.kotlinter") version "3.0.0"
-    id("com.github.ben-manes.versions") version "0.29.0"
-    id("org.springframework.boot") version "2.3.3.RELEASE"
+    id("org.jmailen.kotlinter") version "3.2.0"
+    id("com.github.ben-manes.versions") version "0.33.0"
+    id("org.springframework.boot") version "2.3.4.RELEASE"
     id("org.jetbrains.kotlin.jvm") version kotlinVersion
     id("org.jetbrains.kotlin.plugin.spring") version kotlinVersion
     id("io.spring.dependency-management") version "1.0.10.RELEASE"
