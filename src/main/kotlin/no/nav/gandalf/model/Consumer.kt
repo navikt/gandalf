7ce74437898bcb81df8ad0bf41b3906f42a8f7bd
Youssef Bel Mekki <38552193+ybelMekk@users.noreply.github.com>
2021-01-08 08:58:24 +0100

    add: eksternbruker with propper acr (#33)

--- src/main/kotlin/no/nav/gandalf/model/Consumer.kt
+++ src/main/kotlin/no/nav/gandalf/model/Consumer.kt
@@ -1,5 +1,8 @@
 package no.nav.gandalf.model
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 class Consumer {
     var authority: String? = null
     var ID: String? = null
